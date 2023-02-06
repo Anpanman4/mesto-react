281d2c210e2427953ea529f368a7cdf664f067e9
Aleksey Kondratev <Anpanman4@yandex.ru>
2023-02-06 22:12:41 +0300

    feat: do declare react and header jsx

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './index.css'
+import Header from "./components/Header.js";
+
+function App() {
+  return (
+    <>
+      <Header altText="Логотип Место" />
+    </>
+  );
+}
+
+export default App;
